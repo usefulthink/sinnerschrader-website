938ad8a136bb077166b9de202e18a46c2bdd6288
felkug <felicitas.kugland@sinnerschrader.com>
2016-09-23 10:30:17 +0200

    style(refactor): classnames

--- styleguide/components/module_05_locationsMap/script.js
+++ styleguide/components/module_05_locationsMap/script.js
@@ -4,16 +4,17 @@ function changeLocation() {
 
 	for(var i = 0; i < locationBtn.length; i++){
 		locationBtn[i].addEventListener('click', function() {
+			console.log('click');
 
 			var location = this.getAttribute('data-location');
 			var choosenLocation =  document.querySelectorAll('[data-location="'+ location +'"]');
 
 			for(var i = 0; i < allLocation.length; i++){
-				allLocation[i].classList.remove('isActive');
+				allLocation[i].classList.remove('is-active');
 			}
 
 			for(var x = 0; x < choosenLocation.length; x++) {
-				choosenLocation[x].classList.add('isActive');
+				choosenLocation[x].classList.add('is-active');
 			}
 
 		}, false);
