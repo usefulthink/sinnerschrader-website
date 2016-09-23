4844a0a674dec099b9c0da8ef8ae24c726de3b42
Markus Wolf <markus.wolf@sinnerschrader.com>
2016-09-23 10:06:02 +0200

    update...

--- generator/index.js
+++ generator/index.js
@@ -1,30 +1,14 @@
 const vm = require('vm');
 const path = require('path');
-const ect = require('ect');
 const globby = require('globby');
 const sander = require('sander');
 const camelcase = require('camelcase');
 const uppercaseFirst = require('upper-case-first');
 const babel = require('babel-core');
-
-const React = require('react'); // eslint-disable-line no-unused-vars
+const React = require('react');
 const ReactDOM = require('react-dom/server');
 
 const cssCompiler = require('../server/lib/css-compiler');
-
-const renderer = ect({root: path.join(__dirname, '..'), ext: '.html'});
-
-function renderPages(filepaths) {
-	console.log(`Generating pages...`);
-	return filepaths.map(filepath => renderPage(filepath));
-}
-
-function renderPage(filepath) {
-	const html = renderer.render(path.join(__dirname, '..', filepath));
-	const dest = path.join('dist', filepath.replace(/src\/pages/, ''));
-	console.log(`... writing ${dest}`);
-	return sander.writeFile(dest, html);
-}
 
 function createCss() {
 	console.log(`Generating css...`);
@@ -40,17 +24,6 @@ function createCss() {
 		return sander.writeFile('dist/css/app.css', css);
 	});
 }
-
-function logError(err) {
-	console.error(err);
-	throw err;
-}
-
-globby(['src/pages/**/*.html', '!src/pages/**/*2.html'])
-	.then(filepaths => renderPages(filepaths))
-	.then(() => createCss())
-	.then(() => console.log('Done.'))
-	.catch(err => logError(err));
 
 function createReactComponent(lazyComponentRegistry, filepath, code) {
 	const name = uppercaseFirst(camelcase(path.basename(filepath, '.html')));
@@ -97,37 +70,45 @@ function createReactComponents() {
 		});
 }
 
-globby(['src/pages/**/*2.html'])
-	.then(filepaths => {
-		return Promise.all(filepaths.map(filepath => {
-			return createReactComponents()
-				.then(components => {
-					return sander.readFile(filepath)
-						.then(content => {
-							// Transform JSX
-							const options = {
-								plugins: ['transform-react-jsx']
-							};
-							const pageCode = '__html__ = ' + babel.transform(content.toString(), options).code;
+function renderPages(filepaths, components) {
+	return Promise.all(filepaths.map(filepath => {
+		return sander.readFile(filepath)
+			.then(content => {
+				// Transform JSX
+				const options = {
+					plugins: ['transform-react-jsx']
+				};
+				const pageCode = '__html__ = ' + babel.transform(content.toString(), options).code;
 
-							// Eval JSX
-							const sandbox = Object.assign(
-								{},
-								components,
-								{
-									React,
-									__html__: ''
-								}
-							);
-							vm.runInNewContext(pageCode, sandbox);
+				// Eval JSX
+				const sandbox = Object.assign(
+					{},
+					components,
+					{
+						React,
+						__html__: ''
+					}
+				);
+				vm.runInNewContext(pageCode, sandbox);
 
-							// Render HTML
-							const html = ReactDOM.renderToStaticMarkup(sandbox.__html__);
+				// Render HTML
+				const html = '<!DOCTYPE html>' + ReactDOM.renderToStaticMarkup(sandbox.__html__);
 
-							const dest = path.join('dist', filepath.replace(/src\/pages/, ''));
-							return sander.writeFile(dest, html);
-						});
-				});
-		}));
-	})
+				const dest = path.join('dist', filepath.replace(/src\/pages/, ''));
+				return sander.writeFile(dest, html);
+			});
+	}));
+}
+
+function logError(err) {
+	console.error(err);
+	throw err;
+}
+
+createReactComponents()
+	.then(components =>
+		globby(['src/pages/**/*.html'])
+			.then(filepaths => renderPages(filepaths, components))
+			.then(() => console.log('Done.')))
+	.then(() => createCss())
 	.catch(err => logError(err));
