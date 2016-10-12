7029ff7bd76651892fc92337d4fbec01aeb177e0
Markus Wolf <markus.wolf@sinnerschrader.com>
2016-10-12 10:32:15 +0200

    feat: add maison and noe fonts

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
 console.log('These Are Not the Droids You Are Looking For.');
+
+const FontFaceObserver = require('fontfaceobserver');
+
+new FontFaceObserver('MaisonWEB', {weight: 'bold'}).load().then(() => {
+	document.documentElement.classList.add('maisonweb-loaded'); // eslint-disable-line no-undef
+});
+
+new FontFaceObserver('Noe').load().then(() => {
+	document.documentElement.classList.add('noe-loaded'); // eslint-disable-line no-undef
+});
